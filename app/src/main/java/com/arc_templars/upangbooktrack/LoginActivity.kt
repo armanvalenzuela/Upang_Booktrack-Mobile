20e33dc8974a9b0bf0d251ad54238a1065c5acd9
Valenzuela, Arman Jo B. <arba.valenzuela.up@phinamed.com>
2025-02-10 23:03:14 +0800

    Initial Commit

---
+++ app/src/main/java/com/arc_templars/upangbooktrack/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.arc_templars.upangbooktrack
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username == "admin" && password == "1234") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid login credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
